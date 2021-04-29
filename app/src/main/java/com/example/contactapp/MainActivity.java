dcadaa4974131b95a70956e2dc7af8279efbee7c
LinhLuonLD <linhmk2905@gmail.com>
2021-04-29 19:28:01 +0700

    build source

---
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.contactapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_form);
+        Button btn_createAccount = (Button) findViewById(R.id.btn_createAccount);
+        btn_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Bottom_Nav_Main.class));
+            }
+        });
+    }
+
+}
